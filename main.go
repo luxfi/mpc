bc4f835253ac83049fd2d357487b5df40326aa79
Zach Kelling <z@zeekay.io>
2025-08-16 14:36:40 -0500

    fix: add root main.go to fix lint job
    - Fixes 'no go files to analyze' error
    - Provides entry point documentation
    - Redirects to actual CLI implementations

---
+++ main.go
@@ -0,0 +1,16 @@
+// Package main provides the entry point for the MPC application
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Redirect to the actual CLI implementation
+	fmt.Println("Please use one of the following commands:")
+	fmt.Println("  go run ./cmd/lux-mpc          - Run the MPC node")
+	fmt.Println("  go run ./cmd/lux-mpc-cli      - Run the MPC CLI")
+	fmt.Println("  go run ./cmd/lux-mpc-bridge   - Run the MPC bridge")
+	os.Exit(0)
+}
