4da1e9b0f0bb74fc106b1c057f5251416296224d
Zach Kelling <z@zeekay.io>
2025-12-28 01:13:26 -0800

    feat: configurable WebAuthn RP ID and origins for Liquidity exchange
    Add MPC_WEBAUTHN_RP_ID and MPC_WEBAUTHN_ORIGINS env vars to make
    WebAuthn relying party and origin allowlist configurable. Adds
    Liquidity exchange origins (exchange.{dev,test,main}.example.internal,
    partner) to WebAuthn origin checks, CORS, and security tests.

--- pkg/api/security_regression_test.go
+++ pkg/api/security_regression_test.go
@@ -170,8 +170,13 @@ func TestWebAuthnRequiresValidSignature(t *testing.T) {
 
 func TestWebAuthnOriginValidation(t *testing.T) {
 	allowedOrigins := map[string]bool{
-		"https://lux.network":     true,
-		"https://mpc.lux.network": true,
+		"https://lux.network":                 true,
+		"https://mpc.lux.network":             true,
+		"https://exchange.dev.example.internal":    true,
+		"https://exchange.test.example.internal":   true,
+		"https://exchange.main.example.internal":   true,
+		"https://partner":                true,
+		"https://www.partner":            true,
 	}
 
 	attackOrigins := []string{
@@ -179,6 +184,8 @@ func TestWebAuthnOriginValidation(t *testing.T) {
 		"https://lux.network.evil.com",
 		"http://lux.network",
 		"https://mpc.lux.network.attacker.com",
+		"https://exchange.example.internal.evil.com",
+		"http://partner",
 		"",
 	}
 
