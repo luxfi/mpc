4f6f99673c72082fc43fd0222501d16d6d93bb6c
Zach Kelling <z@zeekay.io>
2026-07-19 19:51:55 -0700

    refactor(backup): move S3 client to hanzoai/s3-go; bump hanzoai/orm 0.5.2->0.6.4
    Replace the abandoned github.com/hanzos3/go-sdk S3 client with the canonical
    github.com/hanzoai/s3-go in pkg/backup/s3.go, and bump hanzoai/orm 0.5.2->0.6.4.
    go.sum completed for hanzoai/s3-go.

--- pkg/backup/s3.go
+++ pkg/backup/s3.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"time"
 
-	s3 "github.com/hanzos3/go-sdk"
-	"github.com/hanzos3/go-sdk/pkg/credentials"
+	s3 "github.com/hanzoai/s3-go"
+	"github.com/hanzoai/s3-go/pkg/credentials"
 
 	"github.com/luxfi/mpc/pkg/kvstore"
 	"github.com/luxfi/mpc/pkg/logger"
